e500275d0660535ee064fb2cbe53bca1c60ba812
leejoonhwan <niip00ng@daum.net>
2021-04-12 15:06:23 +0900

    gen key
    privateKey, address

--- ethereum-sdk/src/main/java/com/gravity9/cardamon/Wallet.java
+++ ethereum-sdk/src/main/java/com/gravity9/cardamon/Wallet.java
@@ -1,9 +1,50 @@
 package com.gravity9.cardamon;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.web3j.crypto.CipherException;
+import org.web3j.crypto.ECKeyPair;
+import org.web3j.crypto.Keys;
+import org.web3j.crypto.WalletFile;
+
+import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+
 public class Wallet {
     public static void main(String[] args) {
-
+        Wallet wallet = new Wallet();
+        wallet.genKey();
     }
 
+    public JsonObject genKey() {
+        JsonObject processJson = new JsonObject();
 
+        try {
+            ECKeyPair ecKeyPair = Keys.createEcKeyPair();
+            BigInteger privateKeyInDec = ecKeyPair.getPrivateKey();
+
+            String sPrivatekeyInHex = privateKeyInDec.toString(16);
+
+            WalletFile aWallet = org.web3j.crypto.Wallet.createLight("password", ecKeyPair);
+            String sAddress = aWallet.getAddress();
+
+            processJson.addProperty("address", "0x" + sAddress);
+            processJson.addProperty("privatekey", sPrivatekeyInHex);
+
+
+        } catch (CipherException e) {
+            //
+        } catch (InvalidAlgorithmParameterException e) {
+            //
+        } catch (NoSuchAlgorithmException e) {
+            //
+        } catch (NoSuchProviderException e) {
+            //
+        }
+
+        System.out.println(new Gson().toJson(processJson));
+        return processJson;
+    }
 }
--- ethereum-sdk/src/test/java/com/gravity9/cardamon/test/EthServiceTest.java
+++ ethereum-sdk/src/test/java/com/gravity9/cardamon/test/EthServiceTest.java
@@ -1,13 +1,54 @@
 package com.gravity9.cardamon.test;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.junit.jupiter.api.Test;
+import org.web3j.crypto.*;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.protocol.http.HttpService;
 
+import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+
 public class EthServiceTest {
 
     private final String URL="HTTP://127.0.0.1:7545";
+
+    @Test
+    public JsonObject getEthWallet() throws Exception
+    {
+        JsonObject processJson = new JsonObject();
+
+        try {
+            ECKeyPair ecKeyPair = Keys.createEcKeyPair();
+            BigInteger privateKeyInDec = ecKeyPair.getPrivateKey();
+
+            String sPrivatekeyInHex = privateKeyInDec.toString(16);
+
+            WalletFile aWallet = Wallet.createLight("password", ecKeyPair);
+            String sAddress = aWallet.getAddress();
+
+            processJson.addProperty("address", "0x" + sAddress);
+            processJson.addProperty("privatekey", sPrivatekeyInHex);
+
+
+        } catch (CipherException e) {
+            //
+        } catch (InvalidAlgorithmParameterException e) {
+            //
+        } catch (NoSuchAlgorithmException e) {
+            //
+        } catch (NoSuchProviderException e) {
+            //
+        }
+
+        System.out.println(new Gson().toJson(processJson));
+        return processJson;
+    }
+
     @Test
     public void getEthClientVersionSync() throws Exception
     {
