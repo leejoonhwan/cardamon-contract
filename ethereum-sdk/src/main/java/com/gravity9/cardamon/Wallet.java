0b8203f034758a27dd7099d0377af70fac8c6fba
leejoonhwan <niip00ng@daum.net>
2021-04-12 14:43:43 +0900

    add web3j conn test code

---
+++ ethereum-sdk/src/main/java/com/gravity9/cardamon/Wallet.java
@@ -0,0 +1,9 @@
+package com.gravity9.cardamon;
+
+public class Wallet {
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ ethereum-sdk/src/test/java/com/gravity9/cardamon/test/EthServiceTest.java
@@ -0,0 +1,38 @@
+package com.gravity9.cardamon.test;
+
+import org.junit.jupiter.api.Test;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.protocol.http.HttpService;
+
+public class EthServiceTest {
+
+    private final String URL="HTTP://127.0.0.1:7545";
+    @Test
+    public void getEthClientVersionSync() throws Exception
+    {
+        Web3j web3j = Web3j.build(new HttpService(URL));
+        Web3ClientVersion web3ClientVersion = web3j.web3ClientVersion().send();
+        System.out.println(web3ClientVersion.getWeb3ClientVersion());
+    }
+
+
+    @Test
+    public void getEthClientVersionASync() throws Exception
+    {
+        Web3j web3 = Web3j.build(new HttpService(URL));  // defaults to http://localhost:8545/
+        Web3ClientVersion web3ClientVersion = web3.web3ClientVersion().sendAsync().get();
+        System.out.println(web3ClientVersion.getWeb3ClientVersion());
+    }
+
+    @Test
+    public void getEthClientVersionRx() throws Exception
+    {
+        Web3j web3 = Web3j.build(new HttpService(URL));
+        web3.web3ClientVersion().flowable().subscribe(x -> {
+            System.out.println(x.getWeb3ClientVersion());
+        });
+
+        Thread.sleep(5000);
+    }
+}
